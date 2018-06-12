725ef074fb14ef9efd9ae33310e5da24b22d41e5
Claudio Orlandi <cso.junior1996@gmail.com>
2018-06-12 16:01:37 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000);
