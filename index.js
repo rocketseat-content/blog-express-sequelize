18306435428a6e7a1298b9b871fd5ef5e3d86849
Claudio Orlandi <cso.junior1996@gmail.com>
2018-06-14 13:49:40 -0300

    Final Application

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const app = express();
 const { User } = require('./app/models');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-User.create({ name: 'Claudio', email: 'claudio@rocketseat.com.br', password: '123456' });
+app.post('/register', async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/find/:id', (req, res) => {
+  res.json('Im in register');
+});
+
+app.get('/findall', (req, res) => {
+  res.json('Im in Find All');
+});
+
+app.put('/update/:id', (req, res) => {
+  res.json('Im in update');
+});
+
+app.delete('/delete/:id', (req, res) => {
+  res.json('Im in delete');
 });
 
 app.listen(3000);
